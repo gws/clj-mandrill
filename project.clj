1d0c50bb673d1d6f9131f5425e35a6e855757bfd
Gordon Stratton <gordon.stratton@gmail.com>
2015-08-22 20:42:14 +0000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :min-lein-version "2.0.0"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[cheshire "5.4.0"]
-                 [clj-http "1.1.2"]]
+  :dependencies [[cheshire "5.5.0"]
+                 [clj-http "2.0.0"]]
   :global-vars {*warn-on-reflection* true}
-  :plugins [[codox "0.8.10"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[codox "0.8.13"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]
                    :plugins [[jonase/eastwood "0.2.1"]]}}
   :test-selectors {:default (complement :integration)
                    :integration :integration})
