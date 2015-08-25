a265ac73ce65f790c416d17e206e0135936e5da8
Gordon Stratton <gordon.stratton@gmail.com>
2015-08-25 21:27:44 +0000

    v0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gws/clj-mandrill "0.2.5"
+(defproject gws/clj-mandrill "0.3.0"
   :description "A Clojure implementation of the Mandrill API"
   :url "https://gitlab.com/gws/clj-mandrill"
   :min-lein-version "2.0.0"
