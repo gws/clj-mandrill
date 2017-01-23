5b830c2cc3bf84a758d9ae72433876e5eabb963e
Gordon Stratton <gordon.stratton@gmail.com>
2017-01-23 17:20:53 +0000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[cheshire "5.6.3"]
+  :dependencies [[cheshire "5.7.0"]
                  [clj-http "3.4.1"]]
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]
                              [lein-codox "0.9.4"]]
